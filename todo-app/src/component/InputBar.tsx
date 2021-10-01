d8cc3f5646223eb22cd3ce14ca33bebef88821bb
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-01 19:19:06 +0900

    feat: InputBar関数の追加
    入力欄を1つのコンポーネントで管理するため

---
+++ todo-app/src/component/InputBar.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Input } from "@chakra-ui/react"
+
+const InputBar = () => {
+    return (
+        <Input placeholder="medium size" size="md" />
+    )
+}
+
+export default InputBar
