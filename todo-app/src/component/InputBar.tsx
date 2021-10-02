b17957cd978c6048243e0c572f57b1c3283c012e
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-10-02 14:24:50 +0900

    feat: 入力された文字の検知機能の追加

--- todo-app/src/component/InputBar.tsx
+++ todo-app/src/component/InputBar.tsx
@@ -1,9 +1,28 @@
 import { Input, VStack } from "@chakra-ui/react"
 
 const InputBar = ()=> {
+  const handleSubmit = () => {
+    console.log("--submit--")
+  }
+
+  const handleInputText: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    console.log(event.target.value);
+  };
+
+
+
   return (
     <VStack pt="20px">
-      <Input w="500px" h="50px" variant="outline" placeholder="input TODO" />
+      <Input 
+        w="500px" 
+        h="50px" 
+        variant="outline" 
+        placeholder="input TODO"
+        onChange={handleInputText}
+        onSubmit={handleSubmit}
+      />
     </VStack>
   )
 }
